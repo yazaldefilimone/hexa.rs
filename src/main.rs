0a994d30ad837c7a1e4bcdbe22224fa1583fb6bd
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-30 23:12:50 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod cli;
+mod repl;
+use cli::command_line;
+use repl::prompt;
+
+// =====================
+// run file(compile) file output result
+//
+fn run(file: &str) {}
+
+// =====================
+// compile file output bytecode
+//
+fn compile(file: &str) {}
+
 fn main() {
-    println!("Hello, world!");
+  let matches = command_line();
+  match matches.subcommand() {
+    Some(("run", sub_matches)) => {
+      let file = sub_matches.get_one::<String>("file").unwrap();
+      run(file);
+    }
+    Some(("compile", sub_matches)) => {
+      let file = sub_matches.get_one::<String>("file").unwrap();
+      compile(file);
+    }
+    _ => prompt(),
+  }
 }
