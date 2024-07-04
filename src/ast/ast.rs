31d33d76923c4d63c46ed02f1be5fb9f465b5a26
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-04 02:56:07 +0200

    *

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,4 +1,6 @@
-use super::tokens::TokenEnum;
+use crate::visitor::Visitor;
+
+use super::tokens::{Token, TokenEnum};
 
 pub enum Expression {
   BinaryExpression(BinaryExpression),
@@ -7,26 +9,45 @@ pub enum Expression {
   Literal(Literal),
 }
 
+impl Expression {
+  pub fn accept<T>(&self, visitor: &mut impl Visitor<T>) -> T {
+    match self {
+      Expression::BinaryExpression(binary_expression) => visitor.visit_binary_expression(binary_expression),
+      Expression::UnaryExpression(unary_expression) => visitor.visit_unary_expression(unary_expression),
+      Expression::GroupingExpression(grouping_expression) => visitor.visit_grouping_expression(grouping_expression),
+      Expression::Literal(literal) => visitor.visit_literal(literal),
+    }
+  }
+}
+
 pub struct BinaryExpression {
-  pub operator: TokenEnum,
+  pub operator: Box<Token>,
   pub left: Box<Expression>,
   pub right: Box<Expression>,
 }
 
 impl BinaryExpression {
-  pub fn new(operator: TokenEnum, left: Expression, right: Expression) -> BinaryExpression {
+  pub fn new(operator: Box<Token>, left: Expression, right: Expression) -> BinaryExpression {
     BinaryExpression { operator, left: Box::new(left), right: Box::new(right) }
+  }
+
+  pub fn accept<T>(&self, visitor: &mut impl Visitor<T>) -> T {
+    visitor.visit_binary_expression(self)
   }
 }
 
 pub struct UnaryExpression {
-  pub operator: TokenEnum,
+  pub operator: Box<Token>,
   pub right: Box<Expression>,
 }
 
 impl UnaryExpression {
-  pub fn new(operator: TokenEnum, right: Expression) -> UnaryExpression {
+  pub fn new(operator: Box<Token>, right: Expression) -> UnaryExpression {
     UnaryExpression { operator, right: Box::new(right) }
+  }
+
+  pub fn accept<T>(&self, visitor: &mut impl Visitor<T>) -> T {
+    visitor.visit_unary_expression(self)
   }
 }
 
@@ -38,6 +59,10 @@ impl GroupingExpression {
   pub fn new(expression: Expression) -> GroupingExpression {
     GroupingExpression { expression: Box::new(expression) }
   }
+
+  pub fn accept<T>(&self, visitor: &mut impl Visitor<T>) -> T {
+    visitor.visit_grouping_expression(self)
+  }
 }
 
 pub enum Literal {
@@ -45,4 +70,10 @@ pub enum Literal {
   NumberLiteral(f64),
   BooleanLiteral(bool),
   NullLiteral,
+}
+
+impl Literal {
+  pub fn accept<T>(&self, visitor: &mut impl Visitor<T>) -> T {
+    visitor.visit_literal(self)
+  }
 }
